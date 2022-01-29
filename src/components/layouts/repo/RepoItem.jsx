b0d39030fb9a1ebe95b4438bdbe5a3331db77124
Rayson Dsouza <rayson789foru@gmail.com>
2022-01-29 10:13:23 +0530

    repo details display

--- src/components/layouts/repo/RepoItem.jsx
+++ src/components/layouts/repo/RepoItem.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {FaEye,FaInfo,FaUtensils,FaLink,FaStar} from 'react-icons/fa'
 
 function RepoItem({repo}) {
-  return <div tabindex="0" class="collapse w-96 border border-primary rounded-box border-base-300 collapse-arrow mt-2"> 
+  const {
+    name,
+    description,
+    html_url,
+    forks,
+    open_issues,
+    stargazers_count,
+    watchers_count
+  }=repo
+  return <div tabindex="0" class="ml-2 mr-2 collapse w-fit border border-primary rounded-box border-base-300 collapse-arrow mt-2"> 
   <div class="collapse-title text-xl font-medium">
-    {repo.name}
+   <a href={html_url}> <FaLink/>{name}</a>
   </div> 
   <div class="collapse-content"> 
-    <p>Collapse content reveals with focus. If you add a checkbox, you can control it using checkbox instead of focus. Or you can force-open/force-close using 
-      <span class="badge badge-outline">collapse-open</span> and 
-      <span class="badge badge-outline">collapse-close</span> classes.
+    <p> {description}
+      <span class="badge badge-outline badge-error mr-2 px-3"><FaEye/>{watchers_count}</span>  
+      <span class="badge badge-outline badge-success mr-2 px-3"><FaStar/>{stargazers_count}</span> 
+      <span class="badge badge-outline badge-info mr-2 px-3"><FaInfo/>{open_issues}</span> 
+      <span class="badge badge-outline badge-warning mr-2 px-3"><FaUtensils/>{forks}</span> 
     </p>
   </div>
 </div> 
